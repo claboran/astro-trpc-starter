af4c6d55b3f3e2492d9ee2439dc98e865a758e77
Christian Laboranowitsch <christian.laboranowitsch@extern.cognizant-mobility.com>
2024-04-21 16:20:04 +0200

    Initial commit

---
+++ src/components/TrpcProviderWrapper.tsx
@@ -0,0 +1,29 @@
+import {useState} from "react";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {trpcReactClient} from "../client";
+import {httpBatchLink} from "@trpc/client";
+import superjson from "superjson";
+import Greeting from "./Greeting.tsx";
+
+const GreetingWrapper = () => {
+    const [queryClient] = useState(() => new QueryClient());
+    const [trpcClient] = useState(() => trpcReactClient.createClient({
+        links: [
+            httpBatchLink({
+                url: '/api/trpc',
+                transformer: {
+                    input: superjson,
+                    output: superjson,
+                },
+            })
+        ],
+    }));
+    return (
+      <trpcReactClient.Provider client={trpcClient} queryClient={queryClient}>
+          <QueryClientProvider client={queryClient}>
+              <Greeting />
+          </QueryClientProvider>
+      </trpcReactClient.Provider>
+    );
+};
+export default GreetingWrapper;
